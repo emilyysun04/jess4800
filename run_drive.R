7e91eea759d115f51d019b5b9761f31cb0193ee5
Sarita Kelkar <xwj3rh@virginia.edu>
2025-01-27 15:46:35 -0500

    Editing run drive file

--- run_drive.R
+++ run_drive.R
@@ -1,59 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-run_drive<-function(state) {
-  #simulate random yardage change (using a uniform distribution)
-  yards <- sample(-5:10,1) #random yardage change (-5 to +10 yards)
-  state$fp<-state$fp + yards #update field position 
-  
-  #check if a score occurred 
-  if (state$fp > 100) {
-    score <- 7 #Touchdown
-  } else if (state$fp > 110) {
-    score <- 3 #field goal 
-  } else {
-    score <- 0 #no score yet 
-  }
-  
-  #Returning the updated state and score
-  return(list(state = state, score = score))
-  }
-}
-
-
-
-
-=======
 # run_drive.R
 run_drive <- function(state) {
   new_fp <- sample(1:100, 1)  # Simulating random field position for now
@@ -69,4 +13,4 @@ check_score <- function(state) {
   return(NA)  # no score - continue the drive
 }
 check_score
->>>>>>> 58734304a0becdc35563bfc1fab4dade3728c0e6
+
