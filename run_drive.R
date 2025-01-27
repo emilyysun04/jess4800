9ab10acf1771b3ff05c77881b0ad495cea97e037
Shruti Mistry <shruti.mistry2427@gmail.com>
2025-01-27 15:29:56 -0500

    Updated

---
+++ run_drive.R
@@ -0,0 +1,15 @@
+# run_drive.R
+run_drive <- function(state) {
+  new_fp <- sample(1:100, 1)  # Simulating random field position for now
+  return(list(down = 1, ytg = 10, fp = new_fp))  # Reset down & yards to go for new possession 
+}
+run_drive
+check_score <- function(state) {
+  if (state$fp >= 100) {
+    return(7)  # touchdown
+  } else if (state$fp >= 110) {
+    return(3)  # field goal
+  }
+  return(NA)  # no score - continue the drive
+}
+check_score
