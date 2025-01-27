a9963c86b481e4caeb89c197c62c8abb4b30de00
Sarita Kelkar <xwj3rh@virginia.edu>
2025-01-27 16:11:10 -0500

    Editing get EP file

--- get_EP.R
+++ get_EP.R
@@ -1,3 +1,4 @@
+# ep_simulation.R
 source("run_epoch.R")
 
 ep_simulation <- function(down, ytg, fp, n = 1000) {
@@ -7,10 +8,6 @@ ep_simulation <- function(down, ytg, fp, n = 1000) {
     scores[i] <- run_epoch(down, ytg, fp)
   }
   
+  print(scores)  # Debugging print for individual scores
   mean(scores)  # Compute expected points
 }
-
-# Run the simulation
-set.seed(42)
-expected_points <- ep_simulation(down = 1, ytg = 10, fp = 50)
-print(expected_points)
