00948b0941c3b5bfcad91da9e9661d6ada233fc8
emilyysun04 <emilyysun04@gmail.com>
2025-01-28 23:00:20 -0500

    removed set seed,updated function names to match from previous files, tried our functions with different field positions

--- get_EP.R
+++ get_EP.R
@@ -1,16 +1,26 @@
 source("run_epoch.R")
 
-ep_simulation <- function(down, ytg, fp, n = 1000) {
-  scores <- numeric(n)
+#function that calculates the expected points 
+#inputs: current down, yards to go for a first down, field position
+get_EP <- function(down, ytg, fp, num_epochs=1000) {
+  #store scores from each series
+  scores <- numeric(num_epochs)
   
-  for (i in 1:n) {
+  for (i in 1:num_epochs) {
+    #simulate one football possession
     scores[i] <- run_epoch(down, ytg, fp)
   }
-  
-  mean(scores)  # Compute expected points
+  #calculate avg of all scores
+  return(mean(scores))
 }
 
-# Run the simulation
-set.seed(42)
-expected_points <- ep_simulation(down = 1, ytg = 10, fp = 50)
-print(expected_points)
+#first down, 10 yards to go, at the 50 yard line
+expected_points <- get_EP(1, 10, 50, num_epochs=1000)
+print(paste("Expected Points:", expected_points))
+#check midfield
+print(get_EP(1, 10, 50, num_epochs=1000))
+#check near redzone
+print(get_EP(1, 10, 80, num_epochs=1000))
+#check when very near scoring
+print(get_EP(1, 10, 95, num_epochs=1000))
+
