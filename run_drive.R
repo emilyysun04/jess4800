ee37fc42b78810f7b221ec2ca06ccca553332ec2
emilyysun04 <emilyysun04@gmail.com>
2025-01-28 22:50:11 -0500

    Added line for min and max field position, making sure it's within the given range. Also commented our code

--- run_drive.R
+++ run_drive.R
@@ -1,15 +1,23 @@
-# run_drive.R
-run_drive <- function(state) {
-  new_fp <- sample(1:100, 1)  # Simulating random field position for now
-  return(list(down = 1, ytg = 10, fp = new_fp))  # Reset down & yards to go for new possession 
+#run drive simulates how far the ball moves on a drive
+run_drive <- function(fp) {
+  #picks random num from -10 to 50 to simulate the yardage gain/loss for each drive
+  yardage_gained <- sample(-10:50, 1)
+  #compute the new field position after the drive
+  #min -> makes sure field position doesn't exceed 120
+  #max -> makes sure field position doesn't drop below 0
+  new_fp <- max(0, min(fp + yardage_gained, 120))
+  #return new field position
+  return(new_fp)
 }
-run_drive
-check_score <- function(state) {
-  if (state$fp >= 100) {
-    return(7)  # touchdown
-  } else if (state$fp >= 110) {
-    return(3)  # field goal
+
+check_score <- function(fp) {
+  #if the new field position is greater than 110, it will be considered a field goal (3 points)
+  if (fp > 110) {
+    return(3) 
+  #if new field position is greater than 100 but less than 110, touchdown (7 points)
+  } else if (fp > 100) {
+    return(7)
   }
-  return(NA)  # no score - continue the drive
+  return(NA_real_)  
 }
-check_score
+
