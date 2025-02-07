846e062dcd24857235ee9900760456810e6740d0
Shruti Mistry <--get>
2025-02-07 10:13:04 -0500

    change run_drive

--- run_drive.R
+++ run_drive.R
@@ -1,24 +1,7 @@
-#run drive simulates how far the ball moves on a drive
-run_drive <- function(fp) {
-  #picks random num from -10 to 50 to simulate the yardage gain/loss for each drive
-  #this distribution will allow for instances where there will sometimes be a score and sometimes there will not be 
-  yardage_gained <- sample(-10:50, 1)
-  #compute the new field position after the drive
-  #min -> makes sure field position doesn't exceed 120
-  #max -> makes sure field position doesn't drop below 0
-  new_fp <- max(0, min(fp + yardage_gained, 120))
-  #return new field position
-  return(new_fp)
-}
-
-check_score <- function(fp) {
-  #if the new field position is greater than 110, it will be considered a field goal (3 points)
-  if (fp > 110) {
-    return(3) 
-  #if new field position is greater than 100 but less than 110, touchdown (7 points)
-  } else if (fp > 100) {
-    return(7)
-  }
-  return(NA_real_)  
+run_drive <- function(down, ytg, fp) {
+  
+  new_fp <- sample(c(80, 105, 115), 1, prob=c(.9, .05, .05))
+  
+  list(down=1, ytg=10, fp=new_fp)
 }
 
