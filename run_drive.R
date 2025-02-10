f321ef0e5d7f4048f65d8dbdca867c1aba9e4cf9
Sarita Kelkar <xwj3rh@virginia.edu>
2025-02-10 15:31:49 -0500

    Modified run_drive.R file by updating drive_state to be within the loop

--- run_drive.R
+++ run_drive.R
@@ -1,12 +1,13 @@
 source("run_play.R")
 
 run_drive <- function(down, ytg, fp) {
-  drive_state <- list(down=down, ytg=ytg, fp=fp, exit_drive=0)
+  drive_state <- list(D = down, YTG = ytg, FP = fp, exit_drive = 0)
   
   while (drive_state$exit_drive == 0) {
-    run_play(drive_state)
+    drive_state <- run_play(drive_state)
   }
   
-  return(list(down=drive_state$down, ytg=drive_state$ytg, fp=drive_state$fp))
+  return(list(down = drive_state$D, ytg = drive_state$YTG, fp = drive_state$FP))
 }
+
 
