67e24b4284515bf37619e702a9be508b94b196d9
Shruti Mistry <--get>
2025-02-14 10:24:57 -0500

    restoring previous version

--- run_drive.R
+++ run_drive.R
@@ -1,21 +1,12 @@
 source("run_play.R")
 
-#runs until a turnover or score
-run_drive <- function(D, YTG, yardline_100) {
-  new_state <- test_run_play(D, YTG, yardline_100)
+run_drive <- function(down, ytg, fp) {
+  drive_state <- list(D = down, YTG = ytg, FP = fp, exit_drive = 0)
   
-  if (new_state$exit_drive == 0) {
-    return(run_drive(new_state$D, new_state$YTG, new_state$yardline_100))
-  } else {
-    new_state
+  while (drive_state$exit_drive == 0) {
+    drive_state <- run_play(drive_state)
   }
+  
+  return(list(down = drive_state$D, ytg = drive_state$YTG, fp = drive_state$FP))
 }
 
-#using the dataset to select starting FP
-sample_row <- pbp_data %>% sample_n(1)
-start_yardline <- sample_row$yardline_100
-
-#running a drive simulation
-result <- run_drive(1, 10, start_yardline)
-
-result
